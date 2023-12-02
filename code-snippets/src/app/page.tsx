f35f3492bc37ba208f634f45689405ab2257e7e3
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-12-02 18:33:30 +0100

    add page to create snippets

--- code-snippets/src/app/page.tsx
+++ code-snippets/src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const first = snippets[1];
+  return (
+    <div>
+      Home Page
+      <div>{first.title}</div>
+    </div>
+  );
 }
--- code-snippets/src/app/snippets/new/page.tsx
+++ code-snippets/src/app/snippets/new/page.tsx
@@ -1,6 +1,22 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    const snippet = await db.snippet.create({
+      data: { title, code },
+    });
+    console.log("serwer");
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -8,7 +24,6 @@ export default function SnippetCreatePage() {
             Title
           </label>
           <input
-            type="text"
             name="title"
             className="border rounder p-2 w-full"
             id="title"
