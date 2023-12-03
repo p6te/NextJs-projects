2c57f874458a8ae7dd60e6e43b8411731d46c583
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-12-03 10:34:24 +0100

    add editSnippetPage

--- code-snippets/src/app/snippets/[id]/page.tsx
+++ code-snippets/src/app/snippets/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/db";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface SnippetShowPageProps {
@@ -20,5 +21,21 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   if (!snippet) {
     return notFound();
   }
-  return <div>{snippet.title}</div>;
+  return (
+    <div className="flex m-4 justify-between items-center">
+      <h1 className="text-xl font-bold"> {snippet.title}</h1>
+      <div className="flex gap-4">
+        <Link
+          className="p-2 border rounded"
+          href={`/snippets/${snippet.id}/edit`}
+        >
+          Edit
+        </Link>
+        <button className="p-2 border rounded">Delete</button>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
