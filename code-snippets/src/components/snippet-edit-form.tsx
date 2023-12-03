6a60358da520edbe6230fb483086333de888e078
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-12-03 20:40:25 +0100

    add server actions to edit and delete snippets

--- code-snippets/src/components/snippet-edit-form.tsx
+++ code-snippets/src/components/snippet-edit-form.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import * as actions from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -15,14 +16,20 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
     setCode(value);
   };
 
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
   return (
-    <Editor
-      height="40vh"
-      theme="vs-dark"
-      language="javascript"
-      defaultValue={snippet.code}
-      options={{ minimap: { enabled: false } }}
-      onChange={handleEditorChange}
-    />
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+      <form action={editSnippetAction}>
+        <button className="p-2 border rounded">Save</button>
+      </form>
+    </div>
   );
 }
