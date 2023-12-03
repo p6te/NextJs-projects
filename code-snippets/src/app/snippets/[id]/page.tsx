adf6e7972b7a4daf1fb1d9323dbaab793db3848f
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-12-03 08:13:40 +0100

    add list of snippets on homePage

---
+++ code-snippets/src/app/snippets/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve("Snippet");
+    }, 2000)
+  );
+
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(props.params.id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+  return <div>{snippet.title}</div>;
+}
