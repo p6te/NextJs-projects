3bb8667bbafdf196c23967757e1df62340c51540
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-12-04 13:07:37 +0100

    add server side validation for create new snippet

--- code-snippets/src/app/snippets/[id]/page.tsx
+++ code-snippets/src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   params: {
@@ -21,6 +22,9 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
   if (!snippet) {
     return notFound();
   }
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
   return (
     <div className="flex m-4 justify-between items-center">
       <h1 className="text-xl font-bold"> {snippet.title}</h1>
@@ -31,7 +35,9 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
         >
           Edit
         </Link>
-        <button className="p-2 border rounded">Delete</button>
+        <form action={deleteSnippetAction}>
+          <button className="p-2 border rounded">Delete</button>
+        </form>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
         <code>{snippet.code}</code>
--- code-snippets/src/app/snippets/new/page.tsx
+++ code-snippets/src/app/snippets/new/page.tsx
@@ -1,46 +1,46 @@
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+"use client";
+
+import { useFormState } from "react-dom";
+import * as actions from "@/actions";
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    "use server";
-
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    const snippet = await db.snippet.create({
-      data: { title, code },
-    });
-    console.log("serwer");
-    redirect("/");
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: "",
+  });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
-          <label htmlFor="title" className="w-12">
+          <label className="w-12" htmlFor="title">
             Title
           </label>
           <input
             name="title"
-            className="border rounder p-2 w-full"
+            className="border rounded p-2 w-full"
             id="title"
           />
         </div>
 
         <div className="flex gap-4">
-          <label htmlFor="code" className="w-12">
+          <label className="w-12" htmlFor="code">
             Code
           </label>
           <textarea
             name="code"
-            className="border rounder p-2 w-full"
+            className="border rounded p-2 w-full"
             id="code"
           />
         </div>
-        <button type="submit" className="rounder p-2 bg-blue-200">
+
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+            {formState.message}
+          </div>
+        ) : null}
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
           Create
         </button>
       </div>
