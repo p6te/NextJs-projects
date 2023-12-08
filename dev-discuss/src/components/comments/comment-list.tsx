c623828096e30f67c2a62ee692a71408a47a8053
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-12-08 19:17:55 +0100

    implement cashing with request memoization

--- dev-discuss/src/components/comments/comment-list.tsx
+++ dev-discuss/src/components/comments/comment-list.tsx
@@ -1,23 +1,19 @@
 import CommentShow from "@/components/comments/comment-show";
-import { type CommentWithAuthor } from "@/db/queries/comments";
+import { fetchCommentByPostId } from "@/db/queries/comments";
 
 interface CommentListProps {
-  fetchData: () => Promise<CommentWithAuthor[]>;
+  postId: string;
 }
 
-export default async function CommentList({ fetchData }: CommentListProps) {
-  const comments = await fetchData();
+export default async function CommentList({ postId }: CommentListProps) {
+  const comments = await fetchCommentByPostId(postId);
 
   const topLevelComments = comments.filter(
     (comment) => comment.parentId === null
   );
   const renderedComments = topLevelComments.map((comment) => {
     return (
-      <CommentShow
-        key={comment.id}
-        commentId={comment.id}
-        comments={comments}
-      />
+      <CommentShow key={comment.id} commentId={comment.id} postId={postId} />
     );
   });
 
